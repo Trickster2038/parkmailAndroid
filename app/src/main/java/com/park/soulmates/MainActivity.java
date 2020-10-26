28ddb9caaac47cbba58df980811a3e42e9141b90
Sergey <33185046+Trickster2038@users.noreply.github.com>
2020-10-26 22:16:56 +0300

    toast_swap_placeholder

--- app/src/main/java/com/park/soulmates/MainActivity.java
+++ app/src/main/java/com/park/soulmates/MainActivity.java
@@ -2,7 +2,14 @@ package com.park.soulmates;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Application;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomMenu = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomMenu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+                                                         @Override
+                                                         public boolean onNavigationItemSelected(final MenuItem menuItem) {
+                                                             String s = "default";
+                                                             switch (menuItem.getItemId()) {
+                                                                 case (R.id.tabFeed):
+                                                                     s = "tab1";
+                                                                     break;
+                                                                 case (R.id.tabMatches):
+                                                                     s = "tab2";
+                                                                     break;
+                                                                 case (R.id.tabProfile):
+                                                                     s = "tab3";
+                                                                     break;
+                                                             }
+                                                             Toast msg = Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG);
+                                                             msg.show();
+                                                             return true;
+                                                         }
+                                                     }
+        );
     }
 }
