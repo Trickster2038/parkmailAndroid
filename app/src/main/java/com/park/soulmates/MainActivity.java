071963241c588d6d1cf711b5a86f52843ba42ce6
Sergey <33185046+Trickster2038@users.noreply.github.com>
2020-10-27 12:24:51 +0300

    auth_firebase_test

--- app/src/main/java/com/park/soulmates/MainActivity.java
+++ app/src/main/java/com/park/soulmates/MainActivity.java
@@ -1,5 +1,6 @@
 package com.park.soulmates;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
@@ -11,8 +12,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +26,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO: this is simple test of firebase-auth, make individual activity later - Serge
+        FirebaseApp.initializeApp(getApplicationContext());
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            // Already signed in
+            // Do nothing
+            Toast msg = Toast.makeText(getApplicationContext(), "already signed", Toast.LENGTH_LONG);
+            msg.show();
+        } else {
+            auth.signInWithEmailAndPassword("fzastahov@gmail.com", "testpass")
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // User signed in successfully
+                                Toast msg = Toast.makeText(getApplicationContext(), "sign as fz..@gmail.com", Toast.LENGTH_LONG);
+                                msg.show();
+                            }
+                        }
+                    });
+        }
+
+
 
         BottomNavigationView bottomMenu = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomMenu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
