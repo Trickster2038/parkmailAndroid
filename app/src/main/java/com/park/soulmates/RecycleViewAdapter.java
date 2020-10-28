3fda6cac353ec128fab789528af9cee698208ab4
Sergey <33185046+Trickster2038@users.noreply.github.com>
2020-10-28 11:54:11 +0300

    simplified firebase feed

--- app/src/main/java/com/park/soulmates/RecycleViewAdapter.java
+++ app/src/main/java/com/park/soulmates/RecycleViewAdapter.java
@@ -1,53 +1,70 @@
+
 package com.park.soulmates;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.park.soulmates.R;
 
-public class RecycleViewAdapter extends RecyclerView.Adapter<RecycleViewAdapter.ViewHolder> {
-    private ArrayList<UserModel> mData;
+// FirebaseRecyclerAdapter is a class provided by 
+// FirebaseUI. it provides functions to bind, adapt and show 
+// database contents in a Recycler View
 
-    public RecycleViewAdapter(ArrayList<UserModel> data) {
-        mData = data;
+public class RecycleViewAdapter extends FirebaseRecyclerAdapter<
+       UserModel, RecycleViewAdapter.personsViewholder> {
+
+    public RecycleViewAdapter(
+            @NonNull FirebaseRecyclerOptions<UserModel> options)
+    {
+        super(options);
     }
 
+    // Function to bind the view in Card view(here 
+    // "person.xml") iwth data in 
+    // model class(here "person.class") 
+    @Override
+    protected void onBindViewHolder(@NonNull personsViewholder holder,
+                     int position, @NonNull UserModel model)
+    {
+
+        // Add firstname from model class (here 
+        // "person.class")to appropriate view in Card 
+        // view (here "person.xml") 
+        holder.bio.setText(model.getBio());
+    }
+
+    // Function to tell the class about the Card view (here 
+    // "person.xml")in 
+    // which the data will be shown 
     @NonNull
     @Override
-    public RecycleViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_feed_profile, parent, false);
-        return new ViewHolder(view);
+    public personsViewholder
+    onCreateViewHolder(@NonNull ViewGroup parent,
+                       int viewType)
+    {
+        View view
+                = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_feed_profile, parent, false);
+        return new RecycleViewAdapter.personsViewholder(view);
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.mProfileDescription.setText(mData.get(position).textDescription);
-        Button likeButton = holder.itemView.findViewById(R.id.likeBtn);
-        likeButton.setOnClickListener(v -> {
-            TextView textUid = holder.itemView.findViewById(R.id.cardUID);
-            LikePush.push(textUid.getText().toString());
-            Log.d("log DB_status", "db_feed - OK");
-        });
-    }
 
-    @Override
-    public int getItemCount() {
-        return mData.size();
-    }
-
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView mProfileDescription;
-
-        public ViewHolder(@NonNull View itemView) {
+    // Sub Class to create references of the views in Crad
+    // view (here "person.xml") 
+    class personsViewholder
+            extends RecyclerView.ViewHolder {
+        TextView bio;
+        public personsViewholder(@NonNull View itemView)
+        {
             super(itemView);
-            mProfileDescription = itemView.findViewById(R.id.profileDescription);
+            bio
+                    = itemView.findViewById(R.id.profileDescription);
         }
     }
 }
--- app/src/main/java/com/park/soulmates/UserModel.java
+++ app/src/main/java/com/park/soulmates/UserModel.java
@@ -1,9 +1,20 @@
 package com.park.soulmates;
 
 public class UserModel {
-    public String textDescription;
+    public String bio;
 
     public UserModel(String textDescription) {
-        this.textDescription = textDescription;
+        this.bio = textDescription;
+    }
+
+    // not used really, but needed to initialize some RecycleView mechanics
+    public UserModel(){this.bio = "no constructor bio";}
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String textDescription) {
+        this.bio = textDescription;
     }
 }
