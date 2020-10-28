e7719a0c0aa189d9513d1d25f309380dd70603a6
TakaiSaisei <takaisaisei@gmail.com>
2020-10-28 15:34:03 +0300

    Attempting to implement a fragment reuse mechanism

--- app/src/main/java/com/park/soulmates/MainActivity.java
+++ app/src/main/java/com/park/soulmates/MainActivity.java
@@ -8,6 +8,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    String TABFEED = "TABFEDD";
+    String TABMATCHES = "TABMATCHES";
+    String TABPROFILE = "TABPROFILE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,18 +21,51 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
             int itemId = item.getItemId();
             if (itemId == R.id.tabFeed) {
-                getSupportFragmentManager().beginTransaction().replace(R.id.placeholder, new FeedFragment()).commit();
-                Log.d("Tab", "Tab 1");
+                if (getSupportFragmentManager().findFragmentByTag(TABFEED) == null) {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.placeholder, new FeedFragment(), TABFEED)
+                            .addToBackStack(TABFEED)
+                            .commit();
+                    Log.d("Tab", "Tab 1");
+                    Log.d("Tab", String.valueOf(getSupportFragmentManager().getFragments()));
+                } else {
+                    getSupportFragmentManager().popBackStack(TABFEED, 0);
+                    Log.d("Tab", "Tab 1 from backstack");
+                }
                 return true;
+
             } else if (itemId == R.id.tabMatches) {
-                getSupportFragmentManager().beginTransaction().replace(R.id.placeholder, new MatchesFragment()).commit();
-                Log.d("Tab", "Tab 2");
+                if (getSupportFragmentManager().findFragmentByTag(TABMATCHES) == null) {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.placeholder, new MatchesFragment(), TABMATCHES)
+                            .addToBackStack(TABMATCHES)
+                            .commit();
+                    Log.d("Tab", "Tab 2");
+                    Log.d("Tab", String.valueOf(getSupportFragmentManager().getFragments()));
+                } else {
+                    getSupportFragmentManager().popBackStack(TABMATCHES, 0);
+                    Log.d("Tab", "Tab 2 from backstack");
+                }
                 return true;
+
             } else if (itemId == R.id.tabProfile) {
-                getSupportFragmentManager().beginTransaction().replace(R.id.placeholder, new ProfileFragment()).commit();
-                Log.d("Tab", "Tab 3");
+                if (getSupportFragmentManager().findFragmentByTag(TABPROFILE) == null) {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.placeholder, new ProfileFragment(), TABPROFILE)
+                            .addToBackStack(TABPROFILE)
+                            .commit();
+                    Log.d("Tab", "Tab 3");
+                    Log.d("Tab", String.valueOf(getSupportFragmentManager().getFragments()));
+                } else {
+                    getSupportFragmentManager().popBackStack(TABPROFILE, 0);
+                    Log.d("Tab", "Tab 3 from backstack");
+                }
                 return true;
             }
+
             return false;
         });
         bottomNavigationView.setSelectedItemId(R.id.tabFeed);
