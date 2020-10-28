c1d3103fcab94b9eb4a701b0e1213af48f6432b0
TakaiSaisei <takaisaisei@gmail.com>
2020-10-28 04:33:42 +0300

    Changing the logic of switching fragments

--- app/src/main/java/com/park/soulmates/MainActivity.java
+++ app/src/main/java/com/park/soulmates/MainActivity.java
@@ -1,17 +1,11 @@
 package com.park.soulmates;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -21,52 +15,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
-        FirebaseDatabase db = FirebaseDatabase.getInstance();
-        DatabaseReference ref = db.getReference("users/".concat(mAuth.getUid().concat("/Bio"))); // Key
-        ref.setValue("This is a test bio-message"); // Value
-        Log.d("log DB_status", "db_main - OK");
-        // end of tests, +- normal code start
-        BottomNavigationView bottomMenu = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-        bottomMenu.setOnNavigationItemSelectedListener(menuItem -> {
-                    String s = "default";
-                    FragmentManager fm;
-                    FragmentTransaction ft;
-
-                    // TODO: make code DRY (use function for transaction)
-                    switch (menuItem.getItemId()) {
-                        case (R.id.tabFeed):
-                            s = "tab1";
-                            FeedFragment feedFragment = new FeedFragment(mAuth);
-                            fm = getFragmentManager();
-                            ft = fm.beginTransaction();
-
-                            ft.replace(R.id.placeholder, feedFragment);
-                            // ft.addToBackStack(null); - will be able in history by backButton
-                            ft.commit();
-                            break;
-                        case (R.id.tabMatches):
-                            s = "tab2";
-                            MatchesFragment matchesFragment = new MatchesFragment();
-                            fm = getFragmentManager();
-                            ft = fm.beginTransaction();
-
-                            ft.replace(R.id.placeholder, matchesFragment);
-                            ft.commit();
-                            break;
-                        case (R.id.tabProfile):
-                            s = "tab3";
-                            ProfileFragment profileFragment = new ProfileFragment();
-                            fm = getFragmentManager();
-                            ft = fm.beginTransaction();
-
-                            ft.replace(R.id.placeholder, profileFragment);
-                            ft.commit();
-                            break;
-                    }
-                    Toast msg = Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG);
-                    // msg.show(); - debug toast for tabs
-                    return true;
-                }
-        );
+        BottomNavigationView bottomNavigationView;
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.tabFeed);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.tabFeed) {
+                getFragmentManager().beginTransaction().replace(R.id.placeholder, new FeedFragment(mAuth)).commit();
+                return true;
+            } else if (itemId == R.id.tabMatches) {
+                getFragmentManager().beginTransaction().replace(R.id.placeholder, new MatchesFragment()).commit();
+                return true;
+            } else if (itemId == R.id.tabProfile) {
+                getFragmentManager().beginTransaction().replace(R.id.placeholder, new ProfileFragment()).commit();
+                return true;
+            }
+            return false;
+        });
     }
 }
