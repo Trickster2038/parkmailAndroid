4e05418707a130a5105597ad22f894ccb5c7113e
TakaiSaisei <takaisaisei@gmail.com>
2020-10-31 03:38:47 +0300

    MainActivity: fix code style issues

--- app/src/main/java/com/park/soulmates/MainActivity.java
+++ app/src/main/java/com/park/soulmates/MainActivity.java
@@ -4,21 +4,19 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int NOTIFY_ID = 103;
+    // private static final int NOTIFY_ID = 103;
     // Идентификатор канала
-    private static String CHANNEL_ID = "t channel";
+    // private static String CHANNEL_ID = "t channel";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("dev_", "main activity started");
+        Log.d("MainActivity", "Main activity started");
 
         NotificationsMatches.startListening(MainActivity.this, MainActivity.class);
 
@@ -28,15 +26,15 @@ public class MainActivity extends AppCompatActivity {
             int itemId = item.getItemId();
             if (itemId == R.id.tabFeed) {
                 getSupportFragmentManager().beginTransaction().replace(R.id.placeholder, new FeedFragment()).commit();
-                Log.d("dev_Tab", "Tab 1");
+                Log.d("MainActivity", "Tab 1");
                 return true;
             } else if (itemId == R.id.tabMatches) {
                 getSupportFragmentManager().beginTransaction().replace(R.id.placeholder, new MatchesFragment()).commit();
-                Log.d("dev_Tab", "Tab 2");
+                Log.d("MainActivity", "Tab 2");
                 return true;
             } else if (itemId == R.id.tabProfile) {
                 getSupportFragmentManager().beginTransaction().replace(R.id.placeholder, new ProfileFragment()).commit();
-                Log.d("dev_Tab", "Tab 3");
+                Log.d("MainActivity", "Tab 3");
 
                 // TODO: explain why we use this scheme
                 return true;
