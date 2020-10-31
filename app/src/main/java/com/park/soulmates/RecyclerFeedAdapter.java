64183c532659ca620f617ccf24952167f5287df9
Sergey <33185046+Trickster2038@users.noreply.github.com>
2020-10-31 21:03:22 +0300

    feed avatars added

--- app/src/main/java/com/park/soulmates/RecyclerFeedAdapter.java
+++ app/src/main/java/com/park/soulmates/RecyclerFeedAdapter.java
@@ -1,17 +1,27 @@
 
 package com.park.soulmates;
 
+import android.net.Uri;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 // FirebaseRecyclerAdapter is a class provided by
 // FirebaseUI. it provides functions to bind, adapt and show
@@ -33,6 +43,32 @@ public class RecyclerFeedAdapter extends FirebaseRecyclerAdapter<
         holder.bio.setText(model.getBio());
         holder.title.setText(model.getName().concat(" ").concat(model.getSurname()));
         holder.uid.setText(model.getUid());
+
+        // URL изображения, который мы получили выше
+        //String url="https://firebasestorage.googleapis.com/v0/b/retrieve-images-958e5.appspot.com/o/9.PNG?alt=media&token=6bd05383-0070-4c26-99cb-dcb17a23f7eb";
+
+        //Glide.with(holder.avatarView).load(url).into(holder.avatarView);
+
+        //FirebaseAuth userAuth = FirebaseAuth.getInstance();
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageReference = storage.getReference().child("users/" + model.getUid() + "/avatar" );
+
+        storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                try {
+                    Glide.with(holder.avatarView).load(uri).into(holder.avatarView);
+                } catch (Exception e){
+                    Log.d("dev_avatar_download", "404");
+                }
+                // Got the download URL for 'users/me/profile.png'
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });
 
         // TODO: disable after click + add simple filter using "gone" view attribute
         Button likeButton = holder.itemView.findViewById(R.id.likeBtn);
@@ -58,9 +94,11 @@ public class RecyclerFeedAdapter extends FirebaseRecyclerAdapter<
     class personsViewholder
             extends RecyclerView.ViewHolder {
         TextView bio, title, uid, interestsField;
+        ImageView avatarView;
         public personsViewholder(@NonNull View itemView)
         {
             super(itemView);
+            avatarView = itemView.findViewById(R.id.feedAvatar);
             bio = itemView.findViewById(R.id.profileBio);
             title = itemView.findViewById(R.id.profileTitle);
             uid  = itemView.findViewById(R.id.cardUID);
