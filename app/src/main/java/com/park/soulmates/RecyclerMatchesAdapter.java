5ac58ca3e308bca9d8d89c068d119b1ac86fba50
TakaiSaisei <takaisaisei@gmail.com>
2020-10-31 04:19:17 +0300

    RecyclerMatchesAdapter: fix code style issues

--- app/src/main/java/com/park/soulmates/RecyclerMatchesAdapter.java
+++ app/src/main/java/com/park/soulmates/RecyclerMatchesAdapter.java
@@ -11,70 +11,66 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-public class RecyclerMatchesAdapter extends FirebaseRecyclerAdapter<
-        MatchModel, RecyclerMatchesAdapter.personsViewholder> {
+public class RecyclerMatchesAdapter extends FirebaseRecyclerAdapter
+        <MatchModel, RecyclerMatchesAdapter.personsViewHolder> {
 
     public RecyclerMatchesAdapter(
-            @NonNull FirebaseRecyclerOptions<MatchModel> options)
-    {
+            @NonNull FirebaseRecyclerOptions<MatchModel> options) {
         super(options);
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull RecyclerMatchesAdapter.personsViewholder holder,
-                                    int position, @NonNull MatchModel model)
-    {
-        FirebaseAuth mAuth = FirebaseAuth.getInstance();
-        DatabaseReference ref =
-                FirebaseDatabase.getInstance().getReference().child("users").child(model.getTargetUID());
-        Log.d("DB_matches_list_target", ref.toString());
-        Log.d("DB_matches_list_account", mAuth.getUid());
+    protected void onBindViewHolder(@NonNull personsViewHolder holder,
+                                    int position, @NonNull MatchModel model) {
+        DatabaseReference ref = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child("users")
+                .child(model.getTargetUID());
+        Log.d("RecyclerMatchesAdapter", ref.toString());
 
         ref.addValueEventListener(new ValueEventListener() {
-                                      @Override
-                                      public void onDataChange(DataSnapshot dataSnapshot) {
-                                          AdvancedUserModel user = dataSnapshot.getValue(AdvancedUserModel.class);
-                                          holder.title.setText(user.getName().concat(" ").concat(user.getSurname()));
-                                          holder.uid.setText(user.getUid());
-                                          holder.contacts.setText(user.getContacts());
-                                          Log.d("DB_matches","listener tick");
-                                      }
-                                      @Override
-                                      public void onCancelled(DatabaseError databaseError) {
-                                          Log.d("The read failed: ", String.valueOf(databaseError.getCode()));
-                                      }
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                AdvancedUserModel user = dataSnapshot.getValue(AdvancedUserModel.class);
+                holder.title.setText(user.getName().concat(" ").concat(user.getSurname()));
+                holder.uid.setText(user.getUid());
+                holder.contacts.setText(user.getContacts());
+                Log.d("RecyclerMatchesAdapter", "listener tick");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("RecyclerMatchesAdapter", String.valueOf(databaseError.getCode()));
+            }
         });
 
     }
 
     @NonNull
     @Override
-    public RecyclerMatchesAdapter.personsViewholder
+    public personsViewHolder
     onCreateViewHolder(@NonNull ViewGroup parent,
-                       int viewType)
-    {
-        View view
-                = LayoutInflater.from(parent.getContext())
+                       int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.fragment_matches_profile, parent, false);
-        return new RecyclerMatchesAdapter.personsViewholder(view);
+        return new personsViewHolder(view);
     }
 
-    class personsViewholder
-            extends RecyclerView.ViewHolder {
+    static class personsViewHolder extends RecyclerView.ViewHolder {
         TextView title, uid, interestsField, contacts;
-        public personsViewholder(@NonNull View itemView)
-        {
+
+        public personsViewHolder(@NonNull View itemView) {
             super(itemView);
             contacts = itemView.findViewById(R.id.profileMatchesContacts);
             title = itemView.findViewById(R.id.profileMatchesTitle);
-            uid  = itemView.findViewById(R.id.cardMatchUID);
+            uid = itemView.findViewById(R.id.cardMatchUID);
             interestsField = itemView.findViewById(R.id.profileMatchesInterests);
         }
     }
