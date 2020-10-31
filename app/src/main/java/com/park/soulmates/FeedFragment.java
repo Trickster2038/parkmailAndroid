fa232f6889706d0fc76a77f4fe6d31547bd06e86
TakaiSaisei <takaisaisei@gmail.com>
2020-10-31 03:30:25 +0300

    FeedFragment: refactoring

--- app/src/main/java/com/park/soulmates/FeedFragment.java
+++ app/src/main/java/com/park/soulmates/FeedFragment.java
@@ -4,53 +4,49 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class FeedFragment extends Fragment {
-    private DatabaseReference mbase;
-    private RecyclerFeedAdapter adapter;
-    //private RecycleViewAdapter mAdapter;
-    private RecyclerView mFeedRecycler;
+    private RecyclerFeedAdapter mAdapter;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        DatabaseReference base = FirebaseDatabase.getInstance().getReference().child("users");
+        FirebaseRecyclerOptions<AdvancedUserModel> options
+                = new FirebaseRecyclerOptions.Builder<AdvancedUserModel>()
+                .setQuery(base, AdvancedUserModel.class)
+                .build();
+        mAdapter = new RecyclerFeedAdapter(options);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_feed, container, false);
-        mFeedRecycler = view.findViewById(R.id.recyclerFeed);
-        mFeedRecycler.setLayoutManager(new LinearLayoutManager(inflater.getContext(), LinearLayoutManager.VERTICAL, false));
-
-        mbase = FirebaseDatabase.getInstance().getReference().child("users");
-        //Log.d("dev_DbRefFeed", mbase.toString());
-
-        FirebaseRecyclerOptions<AdvancedUserModel> options
-                = new FirebaseRecyclerOptions.Builder<AdvancedUserModel>()
-                .setQuery(mbase, AdvancedUserModel.class)
-                .build();
-
-        // Connecting object of required Adapter class to
-        // the Adapter class itself
-        adapter = new RecyclerFeedAdapter(options);
-        // Connecting Adapter class with the Recycler view*/
-        mFeedRecycler.setAdapter(adapter);
+        RecyclerView feedRecycler = view.findViewById(R.id.recyclerFeed);
+        feedRecycler.setLayoutManager(new LinearLayoutManager(inflater.getContext(), LinearLayoutManager.VERTICAL, false));
+        feedRecycler.setAdapter(mAdapter);
         return view;
     }
 
     @Override
-    public void onStart()
-    {
+    public void onStart() {
         super.onStart();
-        adapter.startListening();
+        mAdapter.startListening();
     }
 
     @Override
-    public void onStop()
-    {
+    public void onStop() {
         super.onStop();
-        adapter.stopListening();
+        mAdapter.stopListening();
     }
 }
