2ee0005df5369e739873b0f6ec8e28f6b009b061
TakaiSaisei <takaisaisei@gmail.com>
2020-10-31 03:43:01 +0300

    MatchesFragment: refactoring

--- app/src/main/java/com/park/soulmates/MatchesFragment.java
+++ app/src/main/java/com/park/soulmates/MatchesFragment.java
@@ -5,61 +5,60 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-
-
 public class MatchesFragment extends Fragment {
-    private FirebaseAuth mAuth;
-    private DatabaseReference mbase;
-    private RecyclerMatchesAdapter adapter;
-    private RecyclerView mMatchesRecycler;
+    private RecyclerMatchesAdapter mAdapter;
 
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FirebaseAuth userAuth = FirebaseAuth.getInstance();
+        DatabaseReference databaseReference = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child("users")
+                .child(userAuth.getUid())
+                .child("matches");
+        Log.d("MatchesFragment", databaseReference.toString());
+        FirebaseRecyclerOptions<MatchModel> options
+                = new FirebaseRecyclerOptions
+                .Builder<MatchModel>()
+                .setQuery(databaseReference, MatchModel.class)
+                .build();
+        mAdapter = new RecyclerMatchesAdapter(options);
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_feed, container, false);
-        mMatchesRecycler = view.findViewById(R.id.recyclerFeed);
+        RecyclerView mMatchesRecycler = view.findViewById(R.id.recyclerFeed);
         mMatchesRecycler.setLayoutManager(new LinearLayoutManager(inflater.getContext(), LinearLayoutManager.VERTICAL, false));
-
-        FirebaseAuth userAuth = FirebaseAuth.getInstance();
-        mbase
-                = FirebaseDatabase.getInstance().getReference().child("users").child(userAuth.getUid()).child("matches");
-
-        Log.d("dev_DbRefFeed", mbase.toString());
-
-        FirebaseRecyclerOptions<MatchModel> options
-                = new FirebaseRecyclerOptions.Builder<MatchModel>()
-                .setQuery(mbase, MatchModel.class)
-                .build();
-
-        // Connecting object of required Adapter class to
-        // the Adapter class itself
-        adapter = new RecyclerMatchesAdapter(options);
-        // Connecting Adapter class with the Recycler view*/
-        mMatchesRecycler.setAdapter(adapter);
-
+        mMatchesRecycler.setAdapter(mAdapter);
         return view;
     }
 
     @Override
-    public void onStart()
-    {
+    public void onStart() {
         super.onStart();
-        adapter.startListening();
+        mAdapter.startListening();
     }
 
     @Override
-    public void onStop()
-    {
+    public void onStop() {
         super.onStop();
-        adapter.stopListening();
+        mAdapter.stopListening();
     }
 }
